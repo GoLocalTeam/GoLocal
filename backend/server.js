a596f16726580fb577000fa077768f5b3dd91444
Ashish Rolan <ashishrolan292@gmail.com>
2025-06-28 09:54:26 +0530

    CORS Fixed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 // default route
 app.get('/', (req, res) => {
